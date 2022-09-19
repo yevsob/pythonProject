d924e4fedf9a2029cff3ba9a4079738a4d5d1869
yevsob <yevgeniysobolev@yahoo.com>
2022-09-18 19:11:52 -0700

    first commit

---
+++ ns.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+from math import radians
+from matplotlib import pyplot
+from scipy import stats as sc
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
+
+
+
+
+
---
+++ testing.py
@@ -0,0 +1,2 @@
+def met():
+    return 'happy ending'
